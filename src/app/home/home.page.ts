0e07ec33a9c44db7ce799f26374a27ffbcd6ba55
oubiM <meed.oubi@gmail.com>
2021-12-28 15:57:26 +0100

    updated home page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ModalController } from '@ionic/angular';
+import { DataService } from '../services/data.service';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +10,25 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  private cities = [];
 
-  constructor() {}
+  constructor(private data: DataService,
+    private router: Router,
+    private updateModel: ModalController
+    ) {
+    this.data.getAllCities().snapshotChanges().pipe(
+      map(changes =>
+        changes.map(c =>
+          ({ id: c.payload.key, ...c.payload.val() })
+        )
+      )
+    ).subscribe(data => {
+      this.cities = data;
+      console.log(this.cities[0]['image']);
+    });
+  }
 
+  cityDetail(id) {
+    this.router.navigate([`city-details/${id}`]);
+  }
 }
